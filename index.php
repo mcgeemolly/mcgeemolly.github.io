c65400c59d4ef43defb809742c8ddb7b98d7bd0a
mcgeemolly <mollymcgee316@gmail.com>
2018-03-23 14:55:32 -0500

    php

--- index.php
+++ index.php
@@ -30,25 +30,19 @@
 						<div class="inner">
 
 
-							<!-- Nav -->
-								<nav>
-									<ul>
-										<li><a href="#menu">Menu</a></li>
-									</ul>
-								</nav>
+							<!--Navigation bar-->
+
+							<?
+							php include 'nav.php';
+							?>
+	<!--end of Navigation bar-->
 
 						</div>
 					</header>
 
 					<!-- Menu -->
 						<nav id="menu">
-							<h2>Menu</h2>
-							<ul>
-								<li><a href="#bio">Bio</a></li>
-								<li><a href="#resume">Resume</a></li>
-								<li><a href="#portfolio">Portfolio</a></li>
-								<li><a href="#contact">Contact</a></li>
-							</ul>
+<div w3-include-html="nav.html"></div>
 						</nav>
 
 		<!-- Main -->
---
+++ nav.php
@@ -0,0 +1,9 @@
+<nav id="menu">
+  <h2>Menu</h2>
+  <ul>
+    <li><a href="#bio">Bio</a></li>
+    <li><a href="#resume">Resume</a></li>
+    <li><a href="#portfolio">Portfolio</a></li>
+    <li><a href="#contact">Contact</a></li>
+  </ul>
+</nav>
