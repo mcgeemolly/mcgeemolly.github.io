e66e990d11e042a66ff574da56575ef22b2070d2
Molly McGee <mollymcgee316@gmail.com>
2020-04-24 20:15:58 -0500

    cat site

---
+++ cat/assets/js/script.js
@@ -0,0 +1,12 @@
+/* If you're feeling fancy you can add interactivity
+    to your site with Javascript */
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += "responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
